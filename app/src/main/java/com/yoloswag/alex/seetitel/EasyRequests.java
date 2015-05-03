6070c17ec4d8692b2e9df379cc017f3439c55053
Alex <achoulos@scu.edu>
2015-05-02 17:09:58 -0700

    test commit

--- app/src/main/java/com/yoloswag/alex/seetitel/EasyRequests.java
+++ app/src/main/java/com/yoloswag/alex/seetitel/EasyRequests.java
@@ -3,5 +3,18 @@ package com.yoloswag.alex.seetitel;
 /**
  * Created by Alex on 5/2/15.
  */
+
+//This class is to make the process of GETing and POSTing whistles simpler
 public class EasyRequests {
+
+    public EasyRequests() {
+
+    }
+    public static void getWhistles() {
+
+    }
+    public static EasyRequests getRequests() {
+
+        return new EasyRequests();
+    }
 }
